020773e3de31bde9a3b88c3c71ee04c020b850d5
matricks <matricks@localhost.localdomain>
2015-07-12 14:27:01 +0300

    new commit

--- index.php
+++ index.php
@@ -6,4 +6,5 @@ echo "randul doi, adaugat in devel";
 echo "<br>";
 echo "randul trei, adaugat in devel cu comit si push";
 //sunt in devel si fac merge cu master -> nu asa 
-//sunt in master si fac merge cu devel
+//sunt in master si fac merge cu devel -> a incarca in master din devel
+//fac push ->asa merge
